9071b116886784ffdb0fdffe2b9cacaa7766b94d
developerkorir <caltonkorir45@gmail.com>
2023-03-16 03:07:54 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Main function
+ * put function
+ * return (0) for success
+ */
+
+int main(void)
+{
+	puts("\""Programming is like building a multilingual puzzle");
+	return 0;
+}
