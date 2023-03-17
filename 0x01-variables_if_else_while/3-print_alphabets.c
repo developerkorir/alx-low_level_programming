0bb9d96981f862420de962af08867d7e879e213b
developerkorir <caltonkorir45@gmail.com>
2023-03-17 07:42:44 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - most important part of program
+ * Return: Always return 0 (Success)
+*/
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'Z') {
+			putchar (d);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
