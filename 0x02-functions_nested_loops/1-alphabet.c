243a7404d90104c28b0cd4c2152dbcccb102f06a
developerkorir <caltonkorir45@gmail.com>
2023-03-21 08:37:18 -0700

    alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - most important part of the program
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
