ae999d5fcdf5f8d5c942938db16d85e1653f561e
developerkorir <caltonkorir45@gmail.com>
2023-10-19 03:30:25 -0700

    Add Node

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef _lIST_S_
 #define _LIST_S_
 #include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -19,5 +22,7 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+int _strlen(const char *s);
 
 #endif
